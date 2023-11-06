4372fc41080e92718aa913df60e54d7d8b43cb2c
David Bernstein <susuvnkl6@gmail.com>
2023-11-06 13:40:01 +0200

    changes posts to users

--- routes/lowdbtest.mjs
+++ routes/lowdbtest.mjs
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/test", async (req, res, next) => {
   try {
     // await createDB(); // Invoke the createDB function
-    lowdb.data.posts.push("yes");
+    lowdb.data.users.push("yes");
     await lowdb.write();
     console.log("Database created and updated successfully");
     res.send("Database created and updated successfully");
